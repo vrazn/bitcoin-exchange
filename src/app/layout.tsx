5386d5a1289dced9e62ac77fcb8570544617ed87
Vladislav Raznoschikov <vlad@grantit.io>
2023-01-21 16:24:29 +0800

    Enable Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
-import './globals.css'
+import { AnalyticsWrapper } from '@/components/atoms/analytics';
+
+import './globals.css';
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -12,7 +14,10 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        {children}
+        <AnalyticsWrapper />
+      </body>
     </html>
-  )
+  );
 }
