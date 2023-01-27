7ca579f30e4c85693f1a48102b7cb08e24cf041f
Vladislav Raznoschikov <vlad@grantit.io>
2023-01-27 20:38:18 +0800

    Storybook config for Tailwind Next.js mocks

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,15 @@
+const path = require('path');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  /** Expose public folder to storybook as static */
+  staticDirs: ['../public'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    'storybook-addon-next',
   ],
   framework: '@storybook/react',
   core: {
@@ -14,6 +18,16 @@ module.exports = {
   webpackFinal: async (config) => {
     config.resolve.plugins = config.resolve.plugins ?? [];
     config.resolve.plugins.push(new TsconfigPathsPlugin());
+
+    /**
+     * Fixes font import with /
+     * @see https://github.com/storybookjs/storybook/issues/12844#issuecomment-867544160
+     */
+    config.resolve.roots = [
+      path.resolve(__dirname, '../public'),
+      'node_modules',
+    ];
+
     return config;
   },
 };
